fd3902d17c33befafadd36746d5f7abd164a020b
Joel Colucci <joelcolucci@gmail.com>
2016-07-16 23:14:12 -0400

    refactor: implement min num ops for prim calc

--- week5/primitive_calculator/primitive_calculator.py
+++ week5/primitive_calculator/primitive_calculator.py
@@ -13,93 +13,53 @@ ANSWER 1 more operation because we can use the +1 op
 
 http://d.pr/i/4r7V
 """
+"""
+DPChange(money, coins)
+MinNumCoins(0) <- 0
 
+for m from 1 to money:
+    MinNumCoins(m) <- infinity
+    for i from 1 to |coins|:
+        if m >= coin:
+            NumCoins <- MinNumCoins(m - coin) + 1
+            if NumCoins < MinNumCoins(m):
+                MinNumCoins(m) <- NumCoins
 
+return MinNumCoins(money)
+"""
 
 n = 3
 t = [1, 2, 3, 4, 5, 6, 7, 8]
-o = [1, 1, 1, 2, 3, 2, 3, 4]
+o = [0, 1, 1, 2, 3, 2, 3, 3]
 
 # SEPARATE index AND value were calculating for
 
-def optimal_sequence(n):
+def optimal_sequence(N):
     """return optimal sequence"""
-    # Parallel arrays
-    sequence = [1]
-    min_ops = [0]
+    min_num_ops = [0, 0]
 
-    index = 0
-    current_value = 2
+    for i in range(2, N + 1):
+        min_num_ops.append(min_num_ops[i - 1] + 1)
 
-    while current_value <= n:
-    # Calculate three possible step values
-        print "\nCurrent Value: " + str(current_value)
-        # Possibility #1 - Add one
-        c1_ops = min_ops[index] + 1
-        min_operation = (1, c1_ops)
+        if i % 2 == 0:
+            num_ops = min_num_ops[(i / 2)] + 1
+            if num_ops < min_num_ops[i]:
+                min_num_ops[i] = num_ops
 
-        # Possibility #2 - Multiply by 2
-        if current_value % 2 == 0:
-            c2_ops = min_ops[(current_value / 2) - 1] + 1
+        if i % 3 == 0:
+            num_ops = min_num_ops[(i / 3)] + 1
+            if num_ops < min_num_ops[i]:
+                min_num_ops[i] = num_ops
 
-            if c2_ops < min_operation[1]:
-                min_operation = (2, c2_ops)
-
-        # Possibility #3 - Multiply by 3
-        if current_value % 3 == 0:
-            c3_ops = min_ops[(current_value / 3) - 1] + 1
-
-            if c3_ops < min_operation[1]:
-                min_operation = (3, c3_ops)
-
-        # Write minimum operation
-        if min_operation[0] == 1:
-            print "sequence operation 1"
-            print sequence
-            sequence.append(current_value)
-            min_ops.append(c1_ops)
-
-        elif min_operation[0] == 2:
-            print "sequence operation 2"
-            print "before backtrack"
-            print sequence
-            sequence = sequence[:(current_value / 2) - 1]
-            print "after backtack"
-            print sequence
-
-            sequence.append(current_value)
-            min_ops.append(c2_ops)
-
-        elif min_operation[0] == 3:
-            print "sequence operation 2"
-            print "before backtrack"
-            print sequence
-            sequence = sequence[:(current_value) / 3]
-            print "after backtack"
-            print sequence
-
-            sequence.append(current_value)
-
-            min_ops.append(c3_ops)
-
-
-        current_value += 1
-        index += 1
-
-    # print "index: " + str(index)
-    print min_ops[index]
-    print sequence
-
-    #print "\n"
-    return sequence
-
+    return min_num_ops[N]
 
 
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n = int(input)
-    sequence = list(optimal_sequence(n))
+    print optimal_sequence(n)
+    #sequence = list(optimal_sequence(n))
     # print(len(sequence) - 1)
     # for x in sequence:
     #     print str(x) + " "
