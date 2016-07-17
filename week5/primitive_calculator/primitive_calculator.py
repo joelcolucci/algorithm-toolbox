0062c41f4623a16126777315cdb6fffa0812b49a
Joel Colucci <joelcolucci@gmail.com>
2016-07-17 08:33:10 -0400

    feat: impl algo that passes 1-7 test case, fails 11809

--- week5/primitive_calculator/primitive_calculator.py
+++ week5/primitive_calculator/primitive_calculator.py
@@ -1,4 +1,4 @@
-# Uses python3
+# Uses python2
 import sys
 
 """
@@ -51,15 +51,40 @@ def optimal_sequence(N):
             if num_ops < min_num_ops[i]:
                 min_num_ops[i] = num_ops
 
-    return min_num_ops[N]
+    return create_optimal_sequence(N, min_num_ops)
 
+
+def create_optimal_sequence(N, min_num_ops):
+    sequence = [N]
+    cur_val = N
+    i = 1
+
+    while cur_val > 0:
+        min_op = min_num_ops[cur_val - 1]
+
+        sequence.append(cur_val - 1)
+        updated_val = cur_val - 1
+
+        if cur_val % 2 == 0:
+            if min_num_ops[cur_val / 2] < min_op:
+                sequence[i] = cur_val / 2
+                updated_val = cur_val / 2
+        elif cur_val % 3 == 0:
+            if min_num_ops[cur_val / 3] < min_op:
+                sequence[i] = cur_val / 3
+                updated_val = cur_val / 3
+
+        cur_val = updated_val
+        i += 1
+
+    sequence.reverse()
+    return sequence[1:]
 
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n = int(input)
-    print optimal_sequence(n)
-    #sequence = list(optimal_sequence(n))
-    # print(len(sequence) - 1)
-    # for x in sequence:
-    #     print str(x) + " "
+    sequence = list(optimal_sequence(n))
+    print(len(sequence) - 1)
+    for x in sequence:
+        print str(x) + " "
